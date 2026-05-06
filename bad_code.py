d52446d63f86d740b58bc099e03b4fb3fd20adfc
mdigiovanna <michaeldigiovanna2@gmail.com>
2026-05-06 12:01:01 -0400

    Delete bad code file to test

--- bad_code.py
+++
@@ -1,9 +0,0 @@
-def poorly_formatted(x, y):
-    if x > y:
-        return x
-    else:
-        return y
-
-def another_function(a, b):
-    result = a + b
-    return result
--- tests/test_bad_code.py
+++
@@ -1,6 +0,0 @@
-from bad_code import poorly_formatted
-
-
-def test_poorly_formatted():
-    # Fixed test - should pass now
-    assert poorly_formatted(5, 3) == 5  # Correct!
