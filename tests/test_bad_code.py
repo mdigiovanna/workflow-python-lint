ad28c728b3ac80ebf532b64eead7dcc57dced82e
mdigiovanna <michaeldigiovanna2@gmail.com>
2026-05-06 11:58:56 -0400

    Fixing formatting in tests

--- tests/test_bad_code.py
+++ tests/test_bad_code.py
@@ -1,5 +1,5 @@
-import pytest
 from bad_code import poorly_formatted
+
 
 def test_poorly_formatted():
     # Fixed test - should pass now
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,7 @@
 import pytest
 from calculator import add, multiply, divide
 
+
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
